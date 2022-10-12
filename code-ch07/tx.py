e1eb4da3848eaf8de88beec6f2d84941b0765918
root <jhunjhunwala.uday@gmail.com>
2022-10-12 12:44:07 +0000

    sign input

--- code-ch07/tx.py
+++ code-ch07/tx.py
@@ -219,6 +219,14 @@ class Tx:
         # initialize a new script with [sig, sec] as the cmds
         # change input's script_sig to new script
         # return whether sig is valid using self.verify_input
+        
+        z = self.sig_hash(input_index)
+        der = private_key.sign(z).der()
+        sig = der + SIGHASH_ALL.to_bytes(1,'big')
+        sec = private_key.point.sec()
+        script_sig = Script([sig, sec])
+        self.tx_ins[input_index].script_sig = script_sig
+        return self.verify_input(input_index)
         raise NotImplementedError
 
 
